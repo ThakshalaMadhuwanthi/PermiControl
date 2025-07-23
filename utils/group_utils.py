d1e155e85d129fb524c8350fcb8cffd60afb2ebb
ThakshalaMadhuwanthi <hgtmadhuwanthi@gmail.com>
2025-07-23 00:11:57 -0400

    Create group_utils.py

---
+++ utils/group_utils.py
@@ -0,0 +1,27 @@
+
+import subprocess
+import logging
+from utils.logger import setup_logging
+
+setup_logging()
+
+def create_group(groupname):
+    try:
+        subprocess.run(['sudo', 'groupadd', groupname], check=True)
+        logging.info(f"Group '{groupname}' created successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to create group '{groupname}': {e}")
+
+def delete_group(groupname):
+    try:
+        subprocess.run(['sudo', 'groupdel', groupname], check=True)
+        logging.info(f"Group '{groupname}' deleted successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to delete group '{groupname}': {e}")
+
+def add_user_to_group(username, groupname):
+    try:
+        subprocess.run(['sudo', 'usermod', '-aG', groupname, username], check=True)
+        logging.info(f"User '{username}' added to group '{groupname}' successfully.")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to add user '{username}' to group '{groupname}': {e}")
